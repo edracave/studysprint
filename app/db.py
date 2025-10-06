083fbe870f9a3d26230e6c7975ea280f2e737cbc
Edmundo <tu_correo@ejemplo.com>
2025-10-06 22:11:53 +0200

    Agregar app/db.py y __init__.py para paquetes

---
+++ app/db.py
@@ -0,0 +1,8 @@
+﻿from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+DATABASE_URL = "sqlite+aiosqlite:///./studysprint.db"
+
+Base = declarative_base()
+engine = create_async_engine(DATABASE_URL, future=True, echo=False)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
